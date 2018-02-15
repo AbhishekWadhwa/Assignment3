ab3eb14f6b1ea06a7428c04597c1815fe8bf30de
Abhishek <abhishek.wadhwa2010@gmail.com>
2018-02-15 10:57:51 +0530

    First Stable Version Uploaded

---
+++ includes/js/script.js
@@ -0,0 +1,59 @@
+
+
+$(function() {
+	
+	$('#alertMe').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+	});
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	
+});
+
+
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+var vid = document.getElementById('bgvid');
+vid.onclick = function(e) { video.paused ? video.play() : video.pause(); };
